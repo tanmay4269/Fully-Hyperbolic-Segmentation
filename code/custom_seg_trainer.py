f5427b47622d98d25c63a22868d07ba1d73eddd4
Tanmay Gejapati (asus) <tanmay4269@gmail.com>
2025-06-17 13:52:10 +0530

    added augmentations

--- code/custom_seg_trainer.py
+++ code/custom_seg_trainer.py
@@ -196,12 +196,17 @@ def main():
 
     train_transform = transforms.Compose([
         transforms.Resize(args.img_size),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     val_transform = transforms.Compose([
         transforms.Resize(args.img_size),
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     target_transform = transforms.Compose([
