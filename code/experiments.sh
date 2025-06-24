8852f973189837e4ef48c158ad42bc0674b38ced
Tanmay -- biplab48gb <tanmay4269@gmail.com>
2025-06-24 10:16:30 +0530

    minor

--- code/experiments.sh
+++ code/experiments.sh
@@ -1,3 +1,5 @@
 python segmentation_trainer.py \
     --pretrained-checkpoint-path "archives/classification/checkpoints/best_2025-06-19_10-06-40_L-ResNet18.pth" \
-    --no-wandb 
+    --batch-size=16 \
+    --debug \
+    --wandb-name='debug_adam_b16_l3e-4_pt-backbone'
